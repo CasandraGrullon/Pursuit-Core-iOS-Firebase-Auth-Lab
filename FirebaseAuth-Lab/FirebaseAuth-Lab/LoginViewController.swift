feb0cf68df4ca030a035dc5b1705c2f280f951e7
Casandra <casandragrullon@pursuit.org>
2020-03-02 10:14:15 -0500

    set up view controllers

---
+++ FirebaseAuth-Lab/FirebaseAuth-Lab/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  FirebaseAuth-Lab
+//
+//  Created by casandra grullon on 3/2/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpHereButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func signUpAction(_ sender: UIButton) {
+    }
+    
+    
+}
+
---
+++ FirebaseAuth-Lab/FirebaseAuth-Lab/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  FirebaseAuth-Lab
+//
+//  Created by casandra grullon on 3/2/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
