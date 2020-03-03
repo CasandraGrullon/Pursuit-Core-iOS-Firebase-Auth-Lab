141e85743f05e4db5b687f66fa09cd9c3882a36f
Casandra <casandragrullon@pursuit.org>
2020-03-03 09:54:58 -0500

    profile update works for username but not phone number or email

--- FirebaseAuth-Lab/FirebaseAuth-Lab/ViewControllers/ProfileViewController.swift
+++ FirebaseAuth-Lab/FirebaseAuth-Lab/ViewControllers/ProfileViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
 
@@ -18,8 +19,47 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+        userNameTextField.delegate = self
+        phoneNumberTextField.delegate = self
+        emailAddressTextField.delegate = self
     }
     
+    private func updateUI() {
+        guard let user = Auth.auth().currentUser else {
+            return
+        }
+        emailAddressTextField.text = user.email
+        userNameTextField.text = user.displayName
+        phoneNumberTextField.text = user.phoneNumber
+    }
+    
+    
+    @IBAction func updateProfileButtonAction(_ sender: UIButton) {
+        guard let username = userNameTextField.text, !username.isEmpty, let phoneNumber = phoneNumberTextField.text, !phoneNumber.isEmpty else {
+            showAlert(title: "Missing Fields!", message: "Please fill all required fields")
+            return
+        }
+        
+        let request = Auth.auth().currentUser?.createProfileChangeRequest()
+        
+        request?.displayName = username
+        
+        request?.commitChanges(completion: { [unowned self] (error) in
+            if let error = error {
+                self.showAlert(title: "Error", message: "error changing username \(error)")
+            } else {
+                self.showAlert(title: "Username Updated", message: "successfully updated your username")
+            }
+        })
+        
+    }
+    
+}
+extension ProfileViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
